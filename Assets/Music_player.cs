b2cc788188f415c1035b3fef73524f489323b4ec
LAPTOP-KP6TNU2F\arran games <arranbb2004@gmail.com>
2018-06-30 10:42:16 +0100

    added splashscreen and made camera move

---
+++ Assets/Music_player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_player : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Splashscreencontroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splashscreencontroller : MonoBehaviour {
+    [SerializeField]int levelloadtime;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Startnextscenesequence();
+	}
+
+    private void Startnextscenesequence()
+    {
+        Invoke("Loadnextscene", levelloadtime);
+    }
+    private void Loadnextscene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
