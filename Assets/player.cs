edb6cfd789930e135cb33940eb66b9417d210046
doglol99 <arranbb2004@gmail.com>
2018-07-23 12:58:02 +0100

    calculates offset

--- Assets/player.cs
+++ Assets/player.cs
@@ -5,7 +5,8 @@ using UnityStandardAssets.CrossPlatformInput;
 
 
 public class player : MonoBehaviour {
-
+    [Tooltip("in m/s")] [SerializeField] float xspeed = 4f;
+    [Tooltip("in m/s")][SerializeField] float yspeed = 4f;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,8 +14,9 @@ public class player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float xoffset = xThrow * xspeed * Time.deltaTime;
+        float yoffset = yThrow * yspeed * Time.deltaTime;
 	}
 }
